11cf82da23b7fa280780dd08e49455557fb48da3
kyoo0 <kyoscode@gmail.com>
2020-05-12 02:23:08 -0400

    结构

---
+++ mall/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import jsonp from 'jsonp'
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data(){
+
+  },
+  mounted(){
+     
+  }
+}
+</script>
+
+<style>
+
+</style>
